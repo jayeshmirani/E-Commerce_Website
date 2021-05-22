32940b0fdbfc1c10d68eec2aa4e98e0ab7a41a92
Jayesh Mirani <jayesh.mirani@students.vnit.ac.in>
2021-05-22 20:21:24 +0530

    updated

--- commerce/commerce/settings.py
+++ commerce/commerce/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = 'yoursecretkey'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 <<<<<<< HEAD
